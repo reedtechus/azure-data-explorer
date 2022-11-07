d1fcf1b3cb17feb8e72822ec60b60dafb08dea27
Chris Reed <chris@reedtech.us>
2022-11-07 14:24:44 -0600

    Added connector and request.

---
+++ src/Connectors/AuthConnector.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace ReedTech\AzureAppAuth\Connectors;
+
+use ReedTech\AzureAppAuth\Requests\Auth\FetchTokenRequest;
+use Sammyjo20\Saloon\Http\SaloonConnector;
+use Sammyjo20\Saloon\Traits\Plugins\AcceptsJson;
+
+/**
+ * These methods are 'magic' methods
+ *
+ * @method FetchTokenRequest fetchToken
+ */
+class AuthConnector extends SaloonConnector
+{
+    use AcceptsJson;
+
+    protected array $requests = ['fetchToken' => FetchTokenRequest::class];
+
+    public function __construct(protected string $apiBaseUrl = 'https://login.microsoftonline.com')
+    {
+    }
+
+    /**
+     * The Base URL of the API.
+     *
+     * @return string
+     */
+    public function defineBaseUrl(): string
+    {
+        return $this->apiBaseUrl;
+    }
+
+    /**
+     * The headers that will be applied to every request.
+     *
+     * @return string[]
+     */
+    public function defaultHeaders(): array
+    {
+        return [];
+    }
+
+    /**
+     * The config options that will be applied to every request.
+     *
+     * @return string[]
+     */
+    public function defaultConfig(): array
+    {
+        return [];
+    }
+}
---
+++ src/Requests/Auth/FetchTokenRequest.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace ReedTech\AzureAppAuth\Requests\Auth;
+
+use ReedTech\AzureAppAuth\Connectors\AuthConnector;
+use Sammyjo20\Saloon\Constants\Saloon;
+use Sammyjo20\Saloon\Http\SaloonRequest;
+use Sammyjo20\Saloon\Traits\Plugins\HasFormParams;
+
+class FetchTokenRequest extends SaloonRequest
+{
+    use HasFormParams;
+
+    /**
+     * The connector class.
+     *
+     * @var string|null
+     */
+    protected ?string $connector = AuthConnector::class;
+
+    /**
+     * The HTTP verb the request will use.
+     *
+     * @var string|null
+     */
+    protected ?string $method = Saloon::POST;
+
+    public function __construct(
+        protected string $tenantId,
+        protected string $clientId,
+        protected string $clientSecret,
+        protected string $region,
+        protected string $cluster
+    ) {
+        // Use this if caching is enabled
+        // $this->safeCacheMethods[] = Saloon::POST;
+    }
+
+    /**
+     * The endpoint of the request.
+     *
+     * @return string
+     */
+    public function defineEndpoint(): string
+    {
+        // $tenantID = config('azure-app-auth.tenant_id');
+
+        return "/{$this->tenantId}/oauth2/token";
+    }
+
+    public function defaultData(): array
+    {
+        // $clientID = config('azure-app-auth.client_id');
+        // $clientSecret = config('azure-app-auth.client_secret');
+        // $cluster = config('azure-app-auth.cluster');
+        // $region = config('azure-app-auth.region');
+
+        return [
+            'grant_type' => 'client_credentials',
+            'resource' => "https://{$this->cluster}.{$this->region}.kusto.windows.net/",
+            'client_id' => $this->clientID,
+            'client_secret' => $this->clientSecret,
+        ];
+    }
+}
